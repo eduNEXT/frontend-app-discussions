740463f7bd5b354b5fb47a24ddb2b8ddf61cf8dc
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2024-05-28 11:47:40 +0300

    refactor: code refactoring

--- env.config.js
+++ env.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-    FALSE_VALUE: false,
-    CORRECT_BOOL_VALUE: 'Good, false meant false.  We did not cast a boolean to a string.',
-    INCORRECT_BOOL_VALUE: 'Why was a false boolean true?',
-    INTEGER_VALUE: 123,
     PARAGON_THEME_URLS: {
         core: {
             urls: {
-                default: 'https://cdn.jsdelivr.net/npm/@openedx/paragon@alpha/dist/core.min.css1'
+                default: 'https://cdn.jsdelivr.net/npm/@openedx/paragon@alpha/dist/core.min.css'
             },
         },
         defaults: {
@@ -15,7 +11,7 @@ module.exports = {
         variants: {
             light: {
                 urls: {
-                    default: 'https://cdn.jsdelivr.net/npm/@openedx/paragon@alpha/dist/light.min.css1',
+                    default: 'https://cdn.jsdelivr.net/npm/@openedx/paragon@alpha/dist/light.min.css',
                 },
             },
         },
