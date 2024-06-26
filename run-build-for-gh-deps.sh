67ae425a34f2c719cba2e2a1e0dbdc3432bb7d9a
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2024-06-26 14:23:17 +0300

    refactor: corrected sh

--- run-build-for-gh-deps.sh
+++ run-build-for-gh-deps.sh
@@ -4,7 +4,8 @@ git clone -b dcoa/design-tokens-support --single-branch https://github.com/eduNE
 cd frontend-platform
 npm ci
 npm run build
-rsync -a dist/ ../frontend-platform/
+
+cp -a dist/. ../frontend-platform/
 cd ..
 find frontend-platform -mindepth 1 -maxdepth 1 ! -name 'dist' -delete
 mv frontend-platform/dist/* frontend-platform/
@@ -16,27 +17,12 @@ rm -rf frontend-build
 git clone -b dcoa/design-tokens-support --single-branch https://github.com/eduNEXT/frontend-build.git
 cd frontend-build
 npm ci
-mv dist ../frontend-build-dist
+
+cp -a dist/. ../frontend-build-dist
 cd ..
-rsync -a frontend-build-dist/ frontend-build/
+cp -a frontend-build-dist/. frontend-build/
 rm -rf frontend-build-dist
 cd ../..
-
-cd node_modules/@edx/
-rm -rf frontend-component-header
-git clone -b Peter_Kulko/support-design-tokens --single-branch https://github.com/PKulkoRaccoonGang/frontend-component-header.git
-cd frontend-component-header
-npm i --force
-npm run build
-cd ../../..
-
-cd node_modules/@edx/
-rm -rf frontend-component-header
-git clone -b Peter_Kulko/support-design-tokens --single-branch https://github.com/PKulkoRaccoonGang/frontend-component-header.git
-cd frontend-component-header
-npm i --force
-npm run build
-cd ../../..
 
 cd node_modules/@openedx/
 rm -rf paragon
@@ -45,6 +31,14 @@ cd paragon
 npm pack @openedx/paragon@23.0.0-alpha.2
 tar -xzf openedx-paragon-23.0.0-alpha.2.tgz --strip-components=1
 rm openedx-paragon-23.0.0-alpha.2.tgz
+cd ../../..
+
+cd node_modules/@edx/
+rm -rf frontend-component-header
+git clone -b Peter_Kulko/support-design-tokens --single-branch https://github.com/PKulkoRaccoonGang/frontend-component-header.git
+cd frontend-component-header
+npm i --force
+npm run build
 cd ../../..
 
 fedx-scripts webpack
